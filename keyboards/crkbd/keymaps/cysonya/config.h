c7e49199cf2dca56ba11ab8ccf6ac418c5b3a541
Sonya Chan <cysonya@gmail.com>
2023-04-04 19:18:54 -0400

    add numpad layer

---
+++ keyboards/crkbd/keymaps/cysonya/config.h
@@ -0,0 +1,114 @@
+/*
+This is the C configuration file for the keymap
+
+  Copyright 2022 Mark Stosberg (@markstos)
+  SPDX-License-Identifier: GPL-2.0-or-later
+
+*/
+
+#pragma once
+
+//#define USE_MATRIX_I2C
+
+/* Select hand configuration */
+
+// #define MASTER_LEFT
+#define MASTER_RIGHT
+// #define EE_HANDS
+
+//#define SSD1306OLED
+
+
+// By default, when holding a dual-function key shortly after tapping it, the
+// tapped key will begin repeating. This is handy for fast typists when typing
+// words with double letters, such as "happy". If you turn this setting ON, it
+// will be counted as a held modifier instead.
+//#define TAPPING_FORCE_HOLD
+
+// Customized by markstos
+#define TAPPING_TERM 200
+#define TAPPING_TERM_PER_KEY
+// used for Tapping Term on thumb keys
+#define TAPPING_TERM_THUMB 125
+
+// If you press a dual-role key, press another key, and then release the
+// dual-role key, all within the tapping term, by default the dual-role key
+// will perform its tap action. If the HOLD_ON_OTHER_KEY_PRESS option is
+// enabled, the dual-role key will perform its hold action instead.
+#define HOLD_ON_OTHER_KEY_PRESS
+
+// markstos: not sure if these are correct
+// They are intended to beep and flash during flashing
+#define QMK_LED     D5
+#define QMK_SPEAKER C6
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// When enabled, typing a mod-tap plus second within term will register as the mod-combo
+// Ref: https://beta.docs.qmk.fm/using-qmk/software-features/tap_hold#permissive-hold 
+#define PERMISSIVE_HOLD
+
+#define COMBO_COUNT 2
+
+// Set the COMBO_TERM so low that I won't type the keys one after each other during normal typing.
+// They would have be held together intentionally to trigger this.
+#define COMBO_TERM 40
+
+// These mostly affect my one-shot Shift key, providing a CapsLock alternative.
+// I want a relatively low timeout, so if I accidentally type "Shift", I can pause just briefly and move on.
+#define ONESHOT_TAP_TOGGLE 3  /* Tapping this number of times holds the key until tapped once again. */
+#define ONESHOT_TIMEOUT 2000  /* Time (in ms) before the one shot key is released */
+
+#ifdef RGB_MATRIX_ENABLE
+// #   define RGB_MATRIX_KEYPRESSES // reacts to keypresses
+// #   define RGB_MATRIX_KEYRELEASES // reacts to keyreleases (instead of keypresses)
+#   define RGB_DISABLE_WHEN_USB_SUSPENDED // turn off effects when suspended
+#    undef RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#   define RGB_MATRIX_LED_PROCESS_LIMIT (RGB_MATRIX_LED_COUNT + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
+#   define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
+#   define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150 // limits maximum brightness of LEDs to 150 out of 255. Higher may cause the controller to crash.
+#   define RGB_MATRIX_HUE_STEP 8
+#   define RGB_MATRIX_SAT_STEP 8
+#   define RGB_MATRIX_VAL_STEP 8
+#   define RGB_MATRIX_SPD_STEP 10
+
+/* Disable the animations you don't want/need.  You will need to disable a good number of these    *
+ * because they take up a lot of space.  Disable until you can successfully compile your firmware. */
+#    undef ENABLE_RGB_MATRIX_ALPHAS_MODS
+#   define ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
+#    undef ENABLE_RGB_MATRIX_BREATHING
+#    undef ENABLE_RGB_MATRIX_BAND_SAT
+#    undef ENABLE_RGB_MATRIX_BAND_VAL
+#    undef ENABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
+#    undef ENABLE_RGB_MATRIX_BAND_PINWHEEL_VAL
+#    undef ENABLE_RGB_MATRIX_BAND_SPIRAL_SAT
+#    undef ENABLE_RGB_MATRIX_BAND_SPIRAL_VAL
+#    undef ENABLE_RGB_MATRIX_CYCLE_ALL
+#    undef ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
+#    undef ENABLE_RGB_MATRIX_CYCLE_UP_DOWN
+#    undef ENABLE_RGB_MATRIX_CYCLE_OUT_IN
+#    undef ENABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
+#    undef ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
+#    undef ENABLE_RGB_MATRIX_DUAL_BEACON
+#    undef ENABLE_RGB_MATRIX_CYCLE_PINWHEEL
+#    undef ENABLE_RGB_MATRIX_CYCLE_SPIRAL
+#    undef ENABLE_RGB_MATRIX_RAINBOW_BEACON
+#    undef ENABLE_RGB_MATRIX_RAINBOW_PINWHEELS
+#    undef ENABLE_RGB_MATRIX_RAINDROPS
+#    undef ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
+#    undef ENABLE_RGB_MATRIX_TYPING_HEATMAP
+#    undef ENABLE_RGB_MATRIX_DIGITAL_RAIN
+#    undef ENABLE_RGB_MATRIX_SOLID_REACTIVE
+#    undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+#    undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
+#    undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
+#    undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
+#    undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
+#    undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
+#    undef ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
+#    undef ENABLE_RGB_MATRIX_SPLASH
+#    undef ENABLE_RGB_MATRIX_MULTISPLASH
+#    undef ENABLE_RGB_MATRIX_SOLID_SPLASH
+#    undef ENABLE_RGB_MATRIX_SOLID_MULTISPLASH
+#endif
